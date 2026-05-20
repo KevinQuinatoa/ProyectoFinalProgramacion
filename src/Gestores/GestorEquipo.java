5d38fdc5ef17abb96b40ed0a6e9fd7e195c016d4
Prueba1 <kevin.quinatoa.valladares@udla.edu.ec>
2026-05-19 20:10:40 -0500

    Initial commit

---
+++ src/Gestores/GestorEquipo.java
@@ -0,0 +1,52 @@
+package Registro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorEquipo {
+
+    private List<EquipoComputo> equipoComputo = new ArrayList<>();
+    private List<EquipoImpresion> equipoImpresion = new ArrayList<>();
+    private List<EquipoProyectore> equipoProyectore = new ArrayList<>();
+
+    public void agregarEquipo(DatosEquipo equipo) {
+        if (equipo instanceof EquipoComputo)
+            equipoComputo.add((EquipoComputo) equipo);
+        else if (equipo instanceof EquipoImpresion)
+            equipoImpresion.add((EquipoImpresion) equipo);
+        else if (equipo instanceof EquipoProyectore)
+            equipoProyectore.add((EquipoProyectore) equipo);
+    }
+
+    /** Devuelve todos los equipos en una sola lista (polimorfismo) */
+    public List<DatosEquipo> listarTodos() {
+        List<DatosEquipo> todos = new ArrayList<>();
+        todos.addAll(equipoComputo);
+        todos.addAll(equipoImpresion);
+        todos.addAll(equipoProyectore);
+        return todos;
+    }
+
+    public DatosEquipo buscarPorId(String id) {
+        for (DatosEquipo e : listarTodos())
+            if (e.getId().equals(id)) return e;
+        return null;
+    }
+
+    public void actualizarEstado(String id, String nuevoEstado) {
+        DatosEquipo datosEquipo = buscarPorId(id);
+        if (datosEquipo != null) datosEquipo.setEstado(nuevoEstado);
+    }
+
+    public List<EquipoComputo> getEquipoComputo() {
+        return equipoComputo;
+    }
+
+    public List<EquipoImpresion> getEquipoImpresion() {
+        return equipoImpresion;
+    }
+
+    public List<EquipoProyectore> getEquipoProyectore() {
+        return equipoProyectore;
+    }
+}
